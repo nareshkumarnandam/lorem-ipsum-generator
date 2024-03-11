5f5f8dd46446e0f9aa7ad38e683c6cda17a6f23e
naresh <nareshcena666@gmail.com>
2024-03-12 00:02:10 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import data from "./data";
 
 function App() {
+  const [count , setCount] = useState(0);
+  const [textData, setTextData] = useState([]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+      alert(" Ayyo ! you know that you wrote negative input 😵");
+    }
+    if (count > 8) {
+      amount = 8;
+      alert(
+        " Ayyo ! you are demanding very much paragraph in one go, kindly take little litlle 😀"
+      );
+    }
+    setTextData(data.slice(0, amount));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="form" onSubmit={submitHandler}>
+        <label htmlFor="numberOfParas">paragraphs:</label>
+        <input
+          type="number"
+          id="numberOfParas"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn">Generate</button>
+      </form>
+      <div className="text-generated">
+        {textData.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </div>
     </div>
   );
 }
